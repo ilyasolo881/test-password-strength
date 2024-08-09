5a244b42b8fb09816563537cabc9edd2ea7c48d0
Ilya Solonetskiy <mr.king101@ukr.net>
2024-08-09 13:32:48 +0300

    deploy project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class AppComponent {
-  title = 'angular-user-form';
+  title = 'my-angular-app';
+  password: string = '';
+  strengthClass: string[] = ['gray', 'gray', 'gray'];
+
+  passwordStrength(): void {
+    const minLengthStrenght = this.password.length >= 8;
+    const hasLetters = /[a-zA-Z]/.test(this.password);
+    const hasNumbers = /[0-9]/.test(this.password);
+    const hasSymbols = /[!@#$%^&*(),.?":{}|<>]/.test(this.password);
+
+    if (!minLengthStrenght) {
+      this.strengthClass = ['red', 'red', 'red'];
+    } else if (
+      (hasLetters && !hasNumbers && !hasSymbols) ||
+      (!hasLetters && hasNumbers && !hasSymbols) ||
+      (!hasLetters && !hasNumbers && hasSymbols)
+    ) {
+      this.strengthClass = ['red', 'gray', 'gray'];
+    } else if (
+      (hasLetters && hasNumbers && !hasSymbols) ||
+      (hasLetters && !hasNumbers && hasSymbols) ||
+      (!hasLetters && hasNumbers && hasSymbols)
+    ) {
+      this.strengthClass = ['yellow', 'yellow', 'gray'];
+    } else if (hasLetters && hasNumbers && hasSymbols) {
+      this.strengthClass = ['green', 'green', 'green'];
+    } else {
+      this.strengthClass = ['gray', 'gray', 'gray'];
+    }
+  }
 }
